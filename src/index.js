98f09b87c42d8b3382fd8a5cd066383661018280
unknown <you@example.com>
2020-12-14 02:33:00 -0500

    deploy mymentalstate to github pages

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mentalstate from './Mentalstate';
+import './mental.css';
+import kali from './kali.jpg';
+import Button from 'react-bootstrap/Button';
+//import './index.css';
+//import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.render(<Mentalstate />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+/*  
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+<div className="abuelo">
+
+            <div>Anxiety</div>
+
+            <div>PTSD</div>
+
+            <div>Depression</div>
+
+            <div>Bipolar</div>
+
+            <div>Panic disorder.</div>
+
+            <div>Social Phobia</div>
+
+            <div>Obsessive Compulsive Disorder</div>
+
+            <div>Schizophrenia</div>
+
+            <div>Dementia</div>
+
+            <div>Autism</div>
+
+            
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+*/
